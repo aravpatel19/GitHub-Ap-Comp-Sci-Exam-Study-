e15de8bbbc412d6b18875bdd2c37b95b410fb0fb
aravpatel19 <45393174+aravpatel19@users.noreply.github.com>
2019-05-16 22:53:20 -0400

    interface class created

---
+++ Interface Review/Player.java
@@ -0,0 +1,11 @@
+public interface Player{
+
+	//Player's name
+	public String name(String name);
+
+	//Player's Health
+	public int hp(int health);
+
+	//Maximum amount of Damage that can be dealt
+	public int maxDam(int maxDamage);
+}
